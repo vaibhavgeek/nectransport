9ceefed0bffc1e38f4b2d0016b43b3395de0c17e
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2019-08-24 13:10:12 +0530

    added respective views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def login
+  end
+
+  def form
+  end
+
+  def matches
+  end
+
+  def chat
+  end
+
+  def notify
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,29 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get login" do
+    get home_login_url
+    assert_response :success
+  end
+
+  test "should get form" do
+    get home_form_url
+    assert_response :success
+  end
+
+  test "should get matches" do
+    get home_matches_url
+    assert_response :success
+  end
+
+  test "should get chat" do
+    get home_chat_url
+    assert_response :success
+  end
+
+  test "should get notify" do
+    get home_notify_url
+    assert_response :success
+  end
+
 end
