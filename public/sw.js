a87c615503b3e73f2377aa06c0feacd0d92d3a60
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-08-24 15:28:13 +0530

    installable PWA

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+// CODELAB: Update cache names any time any of the cached files change.
+var CACHE_NAME = 'my-site-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/offline.html',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
