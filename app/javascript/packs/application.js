dde4bb2bc3b19c7b7dba76a318a5c502f06f10c7
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2019-08-25 08:38:50 +0530

    maybe some changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 import './bootstrap_custom.js'
 import '../stylesheets/application'
