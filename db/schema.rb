8a763feebc8fddc3e971ba80490ca823a056186c
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2019-08-25 12:38:29 +0530

    sqlite to postgres change database for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_24_100730) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.string "user"
